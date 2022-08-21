425d9bc5ed7bd286dc02d857e2192c8456d2d90a
Carlos Eduardo Gomes Martins <101645719+MartinsCarlos111@users.noreply.github.com>
2022-08-21 12:13:06 -0300

    Add files via upload

---
+++ Projeto herança/Projeto herança/Atividadedeherençacomirterface/Atividadedeherençacomirterface/Cadastrar Aluno.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Atividadedeherençacomirterface
+{
+    partial class Cadastrar_Aluno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtidadealuno = new System.Windows.Forms.TextBox();
+            this.txtnomealuno = new System.Windows.Forms.TextBox();
+            this.lblidadealuno = new System.Windows.Forms.Label();
+            this.lblnomealuno = new System.Windows.Forms.Label();
+            this.btnvoltaraluno = new System.Windows.Forms.Button();
+            this.btnexibiraluno = new System.Windows.Forms.Button();
+            this.btncadaluno = new System.Windows.Forms.Button();
+            this.txtcurso = new System.Windows.Forms.TextBox();
+            this.lblcurso = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtidadealuno
+            // 
+            this.txtidadealuno.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtidadealuno.Location = new System.Drawing.Point(207, 142);
+            this.txtidadealuno.Name = "txtidadealuno";
+            this.txtidadealuno.Size = new System.Drawing.Size(234, 26);
+            this.txtidadealuno.TabIndex = 9;
+            // 
+            // txtnomealuno
+            // 
+            this.txtnomealuno.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnomealuno.Location = new System.Drawing.Point(207, 107);
+            this.txtnomealuno.Name = "txtnomealuno";
+            this.txtnomealuno.Size = new System.Drawing.Size(234, 26);
+            this.txtnomealuno.TabIndex = 8;
+            // 
+            // lblidadealuno
+            // 
+            this.lblidadealuno.AutoSize = true;
+            this.lblidadealuno.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblidadealuno.Location = new System.Drawing.Point(119, 145);
+            this.lblidadealuno.Name = "lblidadealuno";
+            this.lblidadealuno.Size = new System.Drawing.Size(51, 19);
+            this.lblidadealuno.TabIndex = 7;
+            this.lblidadealuno.Text = "Idade";
+            // 
+            // lblnomealuno
+            // 
+            this.lblnomealuno.AutoSize = true;
+            this.lblnomealuno.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnomealuno.Location = new System.Drawing.Point(121, 107);
+            this.lblnomealuno.Name = "lblnomealuno";
+            this.lblnomealuno.Size = new System.Drawing.Size(54, 19);
+            this.lblnomealuno.TabIndex = 6;
+            this.lblnomealuno.Text = "Nome";
+            // 
+            // btnvoltaraluno
+            // 
+            this.btnvoltaraluno.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnvoltaraluno.Location = new System.Drawing.Point(369, 281);
+            this.btnvoltaraluno.Name = "btnvoltaraluno";
+            this.btnvoltaraluno.Size = new System.Drawing.Size(116, 37);
+            this.btnvoltaraluno.TabIndex = 12;
+            this.btnvoltaraluno.Text = "Voltar";
+            this.btnvoltaraluno.UseVisualStyleBackColor = true;
+            this.btnvoltaraluno.Click += new System.EventHandler(this.btnvoltaraluno_Click);
+            // 
+            // btnexibiraluno
+            // 
+            this.btnexibiraluno.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexibiraluno.Location = new System.Drawing.Point(247, 281);
+            this.btnexibiraluno.Name = "btnexibiraluno";
+            this.btnexibiraluno.Size = new System.Drawing.Size(116, 37);
+            this.btnexibiraluno.TabIndex = 11;
+            this.btnexibiraluno.Text = "Exibir";
+            this.btnexibiraluno.UseVisualStyleBackColor = true;
+            this.btnexibiraluno.Click += new System.EventHandler(this.btnexibiraluno_Click);
+            // 
+            // btncadaluno
+            // 
+            this.btncadaluno.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncadaluno.Location = new System.Drawing.Point(114, 281);
+            this.btncadaluno.Name = "btncadaluno";
+            this.btncadaluno.Size = new System.Drawing.Size(116, 37);
+            this.btncadaluno.TabIndex = 10;
+            this.btncadaluno.Text = "Cadastrar";
+            this.btncadaluno.UseVisualStyleBackColor = true;
+            this.btncadaluno.Click += new System.EventHandler(this.btncadaluno_Click);
+            // 
+            // txtcurso
+            // 
+            this.txtcurso.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcurso.Location = new System.Drawing.Point(207, 183);
+            this.txtcurso.Name = "txtcurso";
+            this.txtcurso.Size = new System.Drawing.Size(234, 26);
+            this.txtcurso.TabIndex = 14;
+            // 
+            // lblcurso
+            // 
+            this.lblcurso.AutoSize = true;
+            this.lblcurso.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcurso.Location = new System.Drawing.Point(119, 186);
+            this.lblcurso.Name = "lblcurso";
+            this.lblcurso.Size = new System.Drawing.Size(56, 19);
+            this.lblcurso.TabIndex = 13;
+            this.lblcurso.Text = "Curso";
+            // 
+            // Cadastrar_Aluno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 450);
+            this.Controls.Add(this.txtcurso);
+            this.Controls.Add(this.lblcurso);
+            this.Controls.Add(this.btnvoltaraluno);
+            this.Controls.Add(this.btnexibiraluno);
+            this.Controls.Add(this.btncadaluno);
+            this.Controls.Add(this.txtidadealuno);
+            this.Controls.Add(this.txtnomealuno);
+            this.Controls.Add(this.lblidadealuno);
+            this.Controls.Add(this.lblnomealuno);
+            this.Name = "Cadastrar_Aluno";
+            this.Text = "Cadastrar_Aluno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtidadealuno;
+        private System.Windows.Forms.TextBox txtnomealuno;
+        private System.Windows.Forms.Label lblidadealuno;
+        private System.Windows.Forms.Label lblnomealuno;
+        private System.Windows.Forms.Button btnvoltaraluno;
+        private System.Windows.Forms.Button btnexibiraluno;
+        private System.Windows.Forms.Button btncadaluno;
+        private System.Windows.Forms.TextBox txtcurso;
+        private System.Windows.Forms.Label lblcurso;
+    }
+}
---
+++ Projeto herança/Projeto herança/Atividadedeherençacomirterface/Atividadedeherençacomirterface/Cadastrar Aluno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividadedeherençacomirterface
+{
+    public partial class Cadastrar_Aluno : Form
+    {
+        Aluno aluno;
+        public Cadastrar_Aluno()
+        {
+            InitializeComponent();
+        }
+
+        private void btncadaluno_Click(object sender, EventArgs e)
+        {
+            aluno = new Aluno(txtnomealuno.Text, (Convert.ToInt32(txtidadealuno.Text)), txtcurso.Text);
+            txtnomealuno.Clear();
+            txtidadealuno.Clear();
+            txtcurso.Clear();
+        }
+
+        private void btnexibiraluno_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(aluno.ImprimeDados());
+        }
+
+        private void btnvoltaraluno_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 principal = new Form1();
+            principal.Show();
+        }
+    }
+}

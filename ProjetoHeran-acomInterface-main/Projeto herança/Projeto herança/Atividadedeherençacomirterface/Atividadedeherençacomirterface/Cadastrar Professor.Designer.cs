152f2efef788d7c0a4f25eea9c12d991222a22de
Carlos Eduardo Gomes Martins <101645719+MartinsCarlos111@users.noreply.github.com>
2022-08-24 11:10:49 -0300

    Add files via upload

---
+++ ProjetoHeran-acomInterface-main/Projeto herança/Projeto herança/Atividadedeherençacomirterface/Atividadedeherençacomirterface/Cadastrar Professor.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Atividadedeherençacomirterface
+{
+    partial class Cadastrar_Professor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnomeprof = new System.Windows.Forms.Label();
+            this.lblidadeprof = new System.Windows.Forms.Label();
+            this.lblTitula = new System.Windows.Forms.Label();
+            this.txtnomeprof = new System.Windows.Forms.TextBox();
+            this.txttitulacao = new System.Windows.Forms.TextBox();
+            this.txtidadeprof = new System.Windows.Forms.TextBox();
+            this.btncadprof = new System.Windows.Forms.Button();
+            this.btnexibirprof = new System.Windows.Forms.Button();
+            this.btnvoltarprof = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblnomeprof
+            // 
+            this.lblnomeprof.AutoSize = true;
+            this.lblnomeprof.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnomeprof.Location = new System.Drawing.Point(102, 83);
+            this.lblnomeprof.Name = "lblnomeprof";
+            this.lblnomeprof.Size = new System.Drawing.Size(54, 19);
+            this.lblnomeprof.TabIndex = 0;
+            this.lblnomeprof.Text = "Nome";
+            // 
+            // lblidadeprof
+            // 
+            this.lblidadeprof.AutoSize = true;
+            this.lblidadeprof.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblidadeprof.Location = new System.Drawing.Point(105, 121);
+            this.lblidadeprof.Name = "lblidadeprof";
+            this.lblidadeprof.Size = new System.Drawing.Size(51, 19);
+            this.lblidadeprof.TabIndex = 1;
+            this.lblidadeprof.Text = "Idade";
+            // 
+            // lblTitula
+            // 
+            this.lblTitula.AutoSize = true;
+            this.lblTitula.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitula.Location = new System.Drawing.Point(102, 157);
+            this.lblTitula.Name = "lblTitula";
+            this.lblTitula.Size = new System.Drawing.Size(79, 19);
+            this.lblTitula.TabIndex = 2;
+            this.lblTitula.Text = "Titulação";
+            // 
+            // txtnomeprof
+            // 
+            this.txtnomeprof.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnomeprof.Location = new System.Drawing.Point(243, 83);
+            this.txtnomeprof.Name = "txtnomeprof";
+            this.txtnomeprof.Size = new System.Drawing.Size(234, 26);
+            this.txtnomeprof.TabIndex = 3;
+            // 
+            // txttitulacao
+            // 
+            this.txttitulacao.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttitulacao.Location = new System.Drawing.Point(243, 154);
+            this.txttitulacao.Name = "txttitulacao";
+            this.txttitulacao.Size = new System.Drawing.Size(234, 26);
+            this.txttitulacao.TabIndex = 4;
+            // 
+            // txtidadeprof
+            // 
+            this.txtidadeprof.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtidadeprof.Location = new System.Drawing.Point(243, 118);
+            this.txtidadeprof.Name = "txtidadeprof";
+            this.txtidadeprof.Size = new System.Drawing.Size(234, 26);
+            this.txtidadeprof.TabIndex = 5;
+            // 
+            // btncadprof
+            // 
+            this.btncadprof.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncadprof.Location = new System.Drawing.Point(106, 245);
+            this.btncadprof.Name = "btncadprof";
+            this.btncadprof.Size = new System.Drawing.Size(116, 37);
+            this.btncadprof.TabIndex = 6;
+            this.btncadprof.Text = "Cadastrar";
+            this.btncadprof.UseVisualStyleBackColor = true;
+            this.btncadprof.Click += new System.EventHandler(this.btncadprof_Click);
+            // 
+            // btnexibirprof
+            // 
+            this.btnexibirprof.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexibirprof.Location = new System.Drawing.Point(239, 245);
+            this.btnexibirprof.Name = "btnexibirprof";
+            this.btnexibirprof.Size = new System.Drawing.Size(116, 37);
+            this.btnexibirprof.TabIndex = 7;
+            this.btnexibirprof.Text = "Exibir";
+            this.btnexibirprof.UseVisualStyleBackColor = true;
+            this.btnexibirprof.Click += new System.EventHandler(this.btnexibirprof_Click);
+            // 
+            // btnvoltarprof
+            // 
+            this.btnvoltarprof.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnvoltarprof.Location = new System.Drawing.Point(361, 245);
+            this.btnvoltarprof.Name = "btnvoltarprof";
+            this.btnvoltarprof.Size = new System.Drawing.Size(116, 37);
+            this.btnvoltarprof.TabIndex = 8;
+            this.btnvoltarprof.Text = "Voltar";
+            this.btnvoltarprof.UseVisualStyleBackColor = true;
+            this.btnvoltarprof.Click += new System.EventHandler(this.btnvoltarprof_Click);
+            // 
+            // Cadastrar_Professor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 356);
+            this.Controls.Add(this.btnvoltarprof);
+            this.Controls.Add(this.btnexibirprof);
+            this.Controls.Add(this.btncadprof);
+            this.Controls.Add(this.txtidadeprof);
+            this.Controls.Add(this.txttitulacao);
+            this.Controls.Add(this.txtnomeprof);
+            this.Controls.Add(this.lblTitula);
+            this.Controls.Add(this.lblidadeprof);
+            this.Controls.Add(this.lblnomeprof);
+            this.Name = "Cadastrar_Professor";
+            this.Text = "Cadastrar_Professor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnomeprof;
+        private System.Windows.Forms.Label lblidadeprof;
+        private System.Windows.Forms.Label lblTitula;
+        private System.Windows.Forms.TextBox txtnomeprof;
+        private System.Windows.Forms.TextBox txttitulacao;
+        private System.Windows.Forms.TextBox txtidadeprof;
+        private System.Windows.Forms.Button btncadprof;
+        private System.Windows.Forms.Button btnexibirprof;
+        private System.Windows.Forms.Button btnvoltarprof;
+    }
+}
---
+++ ProjetoHeran-acomInterface-main/Projeto herança/Projeto herança/Atividadedeherençacomirterface/Atividadedeherençacomirterface/Cadastrar Professor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividadedeherençacomirterface
+{
+    public partial class Cadastrar_Professor : Form
+    {
+        Professor professor;
+        public Cadastrar_Professor()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btncadprof_Click(object sender, EventArgs e)
+        {
+            professor = new Professor(txtnomeprof.Text, (Convert.ToInt32(txtidadeprof.Text)), txttitulacao.Text);
+            txtnomeprof.Clear();
+            txtidadeprof.Clear();
+            txttitulacao.Clear();
+        }
+
+        private void btnexibirprof_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show(professor.ImprimeDados());
+        }
+
+        private void btnvoltarprof_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 principal = new Form1();
+            principal.Show();
+        }
+    }
+}
